7b2ff0a2523327e0f3ab78009d02a2ab70d7e173
TanapornSrisawan <tanaporn.srisa@gmail.com>
2023-03-13 16:31:08 +0700

    Create function

---
+++ function.js
@@ -0,0 +1,26 @@
+// ## Create function following below :</br>
+// add(a, b)</br>
+// subtract(a, b)</br>
+// multiply(a, b)</br>
+// divide(a, b)</br>
+
+const cal = (a,b) => {
+    const add = a+b;  
+    const subtract = a-b;
+    const multiply = a*b;
+    const divide = a/b;
+    console.log('Add = ' + add) ;
+    console.log('Aubtract = ' + subtract) ;
+    console.log('Multiply = ' + multiply) ;
+    console.log('Divide = ' + divide) ;
+}
+cal(10,5);
+
+
+// ## -----another exercise Create Function Inform---</br>
+// console.log("Hi, my name is " + firstName + "I live in " + location + "and enjoy "+hobby);
+
+const inform = (firstName,location,hobby) => {
+    console.log("Hi, my name is " + firstName + ". I live in " + location + " and enjoy "+hobby);
+};
+inform('Ribbin','Vancouve','eating');
